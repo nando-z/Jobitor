bd20b9b976b459f8783d9a782a7f8e5ab22dc5b5
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-05 21:43:12 -0500

    feat: Add reusable search bar component and update job index view

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -5,19 +5,8 @@
             <div class="max-w-md mx-auto">
                 <!-- Centering the content -->
                 <h1 class="text-5xl font-bold">Let's Find You A Great Job</h1>
-
-                <div class="flex justify-center mt-7">
-                    <!-- Centering the input -->
-                    <label class="input flex items-center w-full">
-                        <!-- Added w-full here -->
-                        <svg class="h-[1em] opacity-50 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
-                                <circle cx="11" cy="11" r="8"></circle>
-                                <path d="m21 21-4.3-4.3"></path>
-                            </g>
-                        </svg>
-                        <input type="search" required placeholder="Search" class="input-bordered w-full" /> <!-- Added w-full here -->
-                    </label>
+                <div class=" mt-7">
+                    <x-search-bar />
                 </div>
 
             </div>
