a5bbb05e2d0218890bf2fdc8364cdc3429a41dc7
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-07 08:22:22 -0500

    feat: update registration form layout and enhance title styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-auth-layout>
+<x-layout>
 
 
 
@@ -6,11 +6,13 @@
         <x-forms.title>
             Register
         </x-forms.title>
+        <x-forms.input name="name" label="Your Name" type="email" />
         <x-forms.input name="email" label="Email" />
-        <x-forms.input name="name" label="Your Name" />
+        <x-forms.input name="password" label="Password" type="password" />
+        <x-forms.input name="password_confirmation" label="Password confirmation" type="password" />
         <x-forms.button>
-            Login
+            Create Account
         </x-forms.button>
     </x-forms.form>
 
-</x-auth-layout>
+</x-layout>
--- resources/views/components/forms/title.blade.php
+++ resources/views/components/forms/title.blade.php
@@ -1,1 +1,1 @@
-<h1 class="text-2xl font-bold mt-8">{{ $slot }}</h1>
+<h1 class="text-3xl font-bold mt-8 text-center">{{ $slot }}</h1>
