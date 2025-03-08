de865e96b77f98332bbc13b7bf861db80b9d5c9a
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-08 09:59:50 -0500

    fix: the vailtation

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,12 +6,11 @@ use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Job;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
 
@@ -25,52 +24,34 @@ class JobController extends Controller
         ]);
 
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('job.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(StoreJobRequest $request)
     {
-        //
+        $data = $request->validated(
+            [
+                'title' => ['required', 'string', 'max:255'],
+                'salary' => ['required', 'string', 'max:255'],
+                'location' => ['required', 'string', 'max:255'],
+                'schedule' => ['required', 'string', 'max:255', Rule::in(['Part Time', 'Full Time'])],
+                'url' => ['required', 'active_url', 'max:255'],
+                'tags' => ['nullable'],
+            ]
+        );
+        $data['featured'] = $request->has('featured');
+
+        $job = Auth::user()->employer->jobs()->create([$data]);
+
+        if (isset($data['tags'])) {
+            $job->tags()->attach($data['tags']);
+        }
+
+        return redirect()->route('home')->with('success', 'Job created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Job $job)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Job $job)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateJobRequest $request, Job $job)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Job $job)
-    {
-        //
-    }
 }
